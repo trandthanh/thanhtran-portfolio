1cd952395f53ba87d80abce6e14a7038d93f71a5
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-04-03 00:42:49 +0200

    debug

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -4,13 +4,13 @@ class MessageMailer < ApplicationMailer
   def contact(message)
     @body = message.body
 
-    mail to: "trandgthanh@gmail.com", from: message.email
+    # mail to: "trandgthanh@gmail.com", from: message.email
 
     mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
     message_params = {:from => message.email,
-                      :to => "trandgthanh@gmail.com",
+                      :to => 'trandgthanh@gmail.com',
                       :subject => 'Contact Form',
                       :text => message.body}
-    mg_client.send_message "thanhtran.herokuapp.com", message_params
+    mg_client.send_message 'thanhtran.herokuapp.com', message_params
   end
 end
